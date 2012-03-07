8c50dae1f8defbec5c8fe74c8530f95e1e401298
Jackson Zhang <zbcjackson@gmail.com>
2012-03-07 17:00:23 +0800

    Modify rvm settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,8 @@ require "bundler/capistrano"
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2@TeamColony' # you probably have this already
+set :rvm_type, :user # this is the money config, it defaults to :system
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
