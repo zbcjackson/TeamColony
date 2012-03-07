49fee6155cd4d6f7cf2a4f23c761950b0cace6a3
Jackson Zhang <zbcjackson@gmail.com>
2012-03-07 19:07:46 +0800

    Change mysql sock path

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,7 @@ set :bundle_flags,    "--quiet"
 set :bundle_dir,      ""
 require "bundler/capistrano"
 
+# Consider remove rvm
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2' # you probably have this already
